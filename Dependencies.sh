8183b7962e75bc326a8a92743fa3b64b1c99348f
Matthew Lopez <61769072+matt793@users.noreply.github.com>
2020-12-31 14:27:26 -0700

    Add files via upload

---
+++ Dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update -yy
+sudo apt-get install python3.8 -yy
+sudo apt-get install python3-pip -yy
+pip3 install colorama 
+pip3 install bs4
+pip3 install urllib
+
+exit
